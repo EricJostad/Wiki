e62f9d8e5d54d8c1cea5c1ea9b6aed36b4e6cf35
Eric Jostad <eric.jostad@outlook.com>
2026-07-31 16:09:27 -0700

    Added title function to handle getting entry content via get_entry helper function.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,8 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def title(request, entry):
+    return render(request, "encyclopedia/title.html", {
+        "entry": util.get_entry(entry)
+    })
