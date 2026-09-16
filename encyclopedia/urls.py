70d61011523c742b3155d762dbe27adcce6fb2fe
Eric Jostad <eric.jostad@outlook.com>
2026-09-16 16:40:33 -0700

    Further correcitons to account for 'title' view name change to 'entry'.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:entry>", views.entry, name="title"),
+    path("wiki/<str:entry>", views.entry, name="entry"),
     path("search", views.search, name="search"),
     path("new", views.new, name="new"),
     path("wiki/<str:entry>/edit", views.edit, name="edit"),
