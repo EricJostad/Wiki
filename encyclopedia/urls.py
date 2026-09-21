9e4a7298f2438e6755025a1fa97fdc95c67f2f2c
Eric Jostad <eric.jostad@outlook.com>
2026-09-21 12:41:15 -0700

    Further explanatory comments.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+
+# Added app_name to avoid namespace collisions with other apps in the project. While this may not be important for this project, it is good practice that I want to get in the habit of doing.
+# It also allows for more flexibility in the future if I want to add more apps to the project.
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
